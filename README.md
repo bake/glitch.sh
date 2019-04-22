4da27b7e5e5fc845e1e71ef771a3f5fcb3332b7d
bake <bake@192k.pw>
2019-04-22 12:37:45 +0200

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+# glitch.sh
+
+Create an animated (glitched) GIF of an JPEG image with 5 frames and 5 modified
+bytes each.
+
+```bash
+$ glitch.sh in.jpg out.gif
+$
+```
---
+++ glitch.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+#
+# Create an animated (glitched) GIF of an JPEG image with 5 frames and 5
+# modified bytes each.
+#
+# $ glitch.sh in.jpg out.gif
+#
+
+sos=$(grep -abo $'\xff\xda' "$1" | cut -f1 -d:)
+eoi=$(grep -abo $'\xff\xd9' "$1" | cut -f1 -d:)
+
+cp "$1" "$2"
+for i in {1..5}; do
+	cp "$1" "$1-glitch-$i"
+	for j in {1..5}; do
+		pos=$(shuf -i $sos-$eoi -n 1)
+		printf '\x00' | dd of="$1-glitch-$i" conv=notrunc bs=1 seek=$pos
+	done
+done
+convert $1-glitch-* $2
+rm $1-glitch-*
